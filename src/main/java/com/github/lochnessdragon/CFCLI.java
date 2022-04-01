104769e7e892d4a0ff2b6ae76e5b5568533ae9a7
lochnessdragon <lochnessdragon@github.com>
2022-04-01 02:55:05 -0500

    Added log4j as a dependency

--- src/main/java/com/github/lochnessdragon/CFCLI.java
+++ src/main/java/com/github/lochnessdragon/CFCLI.java
@@ -1,7 +1,12 @@
 package com.github.lochnessdragon;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class CFCLI {
+	public static Logger LOGGER = LogManager.getLogger();
+
 	public static void main(String[] args) {
-		System.out.println("CurseForge CLI Tool version 0.1");
+		LOGGER.info("CurseForge CLI Tool version 0.1");
 	}
 }
